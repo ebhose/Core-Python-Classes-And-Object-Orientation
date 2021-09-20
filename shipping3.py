46dcadb01759fdd45ec12e2f28fe76a04dac6d37
ebhose <ebhose@local.home>
2021-09-20 12:14:07 +0200

    Add class HeatedRefrigeratedShippingContainer to file shipping3.py

--- shipping3.py
+++ shipping3.py
@@ -7,6 +7,9 @@ class ShippingContainer:
 
     next_serial = 1337
 
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+
 
     @classmethod
     def _generate_serial(cls):
@@ -22,17 +25,112 @@ class ShippingContainer:
         )
 
     @classmethod
-    def create_empty(cls, owner_code):
-        return cls(owner_code, contents=[])
+    def create_empty(cls, owner_code, length_ft, **kwargs):
+        return cls(owner_code, length_ft, contents=[], **kwargs)
 
     @classmethod
-    def create_with_items(cls, owner_code, items):
-        return cls(owner_code, contents=list(items))
+    def create_with_items(cls, owner_code, length_ft, items, **kwargs):
+        return cls(owner_code, length_ft, contents=list(items), **kwargs)
 
-    def __init__(self, owner_code, contents):
+    # This is how we define a getter method in python
+    # With the property decorator
+    @property
+    def bic(self):
+        return self._bic
+
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.contents = contents
-        self.bic = ShippingContainer._make_bic_code(
+        self._bic = self._make_bic_code(
             owner_code=owner_code,
             serial=ShippingContainer._generate_serial()
         )
+
+    @property
+    def volume_ft3(self):
+        return self._calc_volume()
+
+    def _calc_volume(self):
+        return (
+            ShippingContainer.HEIGHT_FT *
+            ShippingContainer.WIDTH_FT *
+            self.length_ft
+        )
+
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME_FT3 = 100
+
+
+    def __init__(self, owner_code, length_ft, contents, *, celsius, **kwargs):
+        super().__init__(owner_code, length_ft, contents, **kwargs)
+
+        # The self.celsius attribute will be set by the setter method
+        # We only need to do the check in the setter method this way
+        self.celsius = celsius
+
+    @staticmethod
+    def _c_to_f(celsius):
+        return celsius * 9 / 5 + 32
+
+    @staticmethod
+    def _f_to_c(fahrenheit):
+        return (fahrenheit - 32) * 5 / 9
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    # Good OOP practice is to not override the properties directly
+    # Make regular methods and override those methods in the
+    # Child class. So override _set_celsius in the Child class
+    @celsius.setter
+    def celsius(self, value):
+        self._set_celsius(value)
+
+    def _set_celsius(self, value):
+        if value > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot!")
+        self._celsius = value
+
+    @property
+    def fahrenheit(self):
+        return RefrigeratedShippingContainer._c_to_f(self.celsius)
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = RefrigeratedShippingContainer._f_to_c(value)
+
+    def _calc_volume(self):
+        return (
+            super()._calc_volume() -
+            RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
+        )
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code = owner_code,
+            serial = str(serial).zfill(6),
+            category = "R"
+        )
+
+
+class HeatedRefrigeratedShippingContainer(RefrigeratedShippingContainer):
+
+    MIN_CELSIUS = -20
+
+
+    def _set_celsius(self, value):
+        if value < HeatedRefrigeratedShippingContainer.MIN_CELSIUS:
+            raise ValueError("Temperature too cold!")
+        super()._set_celsius(value)
+
+
+
+
+
+
